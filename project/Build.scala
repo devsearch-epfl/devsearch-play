788ff4f26af943b1dda5e8e7f4dc4d9efbdc3a51
Pascal Lau <pascal.lau91@gmail.com>
2015-04-17 13:51:37 +0200

    Dependency issue on Travis should be solved

--- project/Build.scala
+++ project/Build.scala
@@ -5,8 +5,8 @@ import sbt._
 object MyBuild extends Build {
 
   lazy val root = Project("root", file(".")).dependsOn(astParser).enablePlugins(PlayScala)
-  //lazy val astParser = RootProject(uri("git://github.com/devsearch-epfl/devsearch-ast.git"))
-  lazy val astParser = RootProject(file("../devsearch-ast"))
+  lazy val astParser = RootProject(uri("git://github.com/devsearch-epfl/devsearch-ast.git#"+astCommit))
+  //lazy val astParser = RootProject(file("../devsearch-ast"))
   lazy val astCommit = "f2940793f16c7f006d93c1d1dcf8a908ed21c65c"
 
   /*
