9be2bbf746ea77581d7a8078ae5e91f7ca638140
Damien Engels <engelsdamien@gmail.com>
2015-04-30 07:37:00 +0200

    Added visualisation of advanced search functionality

--- public/javascript/init.js
+++ public/javascript/init.js
@@ -4,12 +4,28 @@ SyntaxHighlighter.all()
 
 $(function(){
 
+    // Side bar
     $('.button-collapse').sideNav();
 
+    // Spinning wheel on search button
     $('#search-btn').click(function(){
         $('#search-btn').hide()
         $('#search-spin').show()
     })
+
+    // Toggle search
+    $('#toggle-filters').children("a").click(function()
+    {
+        if($(".language-filter:first").is(":hidden")) {
+            $(".language-filter").show("slow")
+            $('#toggle-filters').children("a").text("Hide")
+        } else {
+            $(".language-filter").hide("slow")
+            $('#toggle-filters').children("a").text("Advanced search")
+        }
+
+        return false;
+    });
 
 }); // end of document ready
 
