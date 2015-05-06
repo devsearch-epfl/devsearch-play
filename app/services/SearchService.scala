25964de55fe0b46e2d3142cd7da98b24ac88b58d
julien graisse <julien.graisse@epfl.ch>
2015-05-06 19:10:37 +0200

    The front-end now prints detected features and recognized language of the query

--- app/services/SearchService.scala
+++ app/services/SearchService.scala
@@ -21,22 +21,21 @@ object SearchService {
   val initialContacts = Set(Akka.system.actorSelection("akka.tcp://lookupCluster@127.0.0.1:2555/user/receptionist"))
   val clusterClient = Akka.system.actorOf(ClusterClient.props(initialContacts), "clusterClient")
 
-  def get(query: String, maxDuration: FiniteDuration): Future[SearchResult] = {
+  def get(query: String, maxDuration: FiniteDuration): (Option[String],List[String], Future[SearchResult]) = {
     Logger.info("--- new query ---")
     Logger.info("Input: " + query)
 
     implicit val timeout = new Timeout(maxDuration)
 
-    val featureKeys = QueryRecognizer(query) match {
+    val (detectedLanguage, featureKeys) = QueryRecognizer(query) match {
       case Some(codeFile) =>
-        val detectedLanguage = codeFile.language
-        Logger.info("Detected language for query: " + detectedLanguage)
-        FeatureRecognizer(codeFile).map(_.key).toList
-      case _ => List[String]()
+        Logger.info("Detected language for query: " + codeFile.language)
+        (Some(codeFile.language), FeatureRecognizer(codeFile).map(_.key).toList)
+      case _ => (None, List[String]())
     }
-
     Logger.info("Features: " + featureKeys.size)
-    featureKeys.zipWithIndex.foreach { case (feature, idx) => Logger.info(s" ${idx + 1}. $feature") }
+    featureKeys.zipWithIndex.foreach { case (feature, idx) =>  Logger.info(s" ${idx + 1}. $feature") }
+    val featureList: List[String] = featureKeys.zipWithIndex.map( x => (s"Feature ${x._2 + 1}: ${x._1}") )
 
     val results = (clusterClient ? ClusterClient.Send("/user/lookup", SearchRequest(featureKeys), localAffinity = true))
       .collect { case s: SearchResult => s }
@@ -48,7 +47,7 @@ object SearchService {
       case SearchResultError(message) => Logger.error(s"error: $message")
       case SearchResultSuccess(entries) => Logger.info(s"received: ${entries.length} entries")
     }
-    results
+    (detectedLanguage,featureList, results)
   }
 
 }
