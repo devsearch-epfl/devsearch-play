ef90eb855e210570a16f8e990fdcf4be41f5ec6b
Damien Engels <engelsdamien@gmail.com>
2015-05-08 00:51:30 +0200

    Truncate path and nicer bar for code snippets

--- app/models/Results.scala
+++ app/models/Results.scala
@@ -8,6 +8,18 @@ import devsearch.parsers.Languages
 case class SnippetResult(user: String, repo: String, path: String, startLine: Int, endLine : Int, code : Option[String]){
   def previewUrl = s"https://github.com/$user/$repo/blob/master/${path}#L$startLine-L$endLine"
   def language = Languages.guess(path).map(_.toLowerCase)
+
+  def truncadedPath = if (path.length > 50){
+   path.lastIndexOf("/") match {
+     case -1 => path
+     case i =>
+       val (prefix, filename) = path.splitAt(i)
+       val shorter = prefix.take(50 - filename.length).reverse.dropWhile(_ != '/').reverse
+       shorter + "..." + filename
+   }
+  } else {
+    path
+  }
 }
 
 case class QueryInfo(query : String, detectedLang : Option[String], features : Set[String])
