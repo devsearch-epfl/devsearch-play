ccfc31ab2916163ac96be0eba97994020be9b734
Damien Engels <engelsdamien@gmail.com>
2015-05-07 23:23:14 +0200

    Put back actions on cards

--- app/models/Results.scala
+++ app/models/Results.scala
@@ -4,7 +4,7 @@ package models
  * Created by dengels on 07/05/15.
  */
 case class SnippetResult(user: String, repo: String, path: String, startLine: Int, endLine : Int, code : Option[String]){
-  def previewUrl = s"https://github.com/$user/$repo/blob/master/${path}L$startLine-L$endLine"
+  def previewUrl = s"https://github.com/$user/$repo/blob/master/${path}#L$startLine-L$endLine"
 }
 
 case class QueryInfo(query : String, detectedLang : Option[String], features : Set[String])
